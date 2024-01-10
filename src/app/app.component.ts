a0510495ab659e9a495aa3f67c3a45b597fd04ee
Sarangan Elangovan <sarangan.e@tvsnext.com>
2024-01-10 15:44:55 +0530

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'photo-gallery';
+
+  title = 'Photo Gallery';
+
+  TableData:any
+  TotalImages:number=0
+  Pages:number=0
+  SearchWord:string=''
+  Pagenumber:number=1
+  ItemsPerPage:number=5
+  client_id='_lhDJVLYdOaEoNcZV1tmY3BtNCCuol-2Cq1vjwUxqRg'
+
+  constructor(private http:HttpClient) { }
+
+  search()
+  {
+
+  this.http.get('https://api.unsplash.com/search/photos?query='+this.SearchWord+',&page='+this.Pagenumber+',&per_page='+this.ItemsPerPage+',&client_id='+this.client_id+'').subscribe(
+
+    (res:any)=>
+    {
+      const results='results' ; const total='total'; const total_pages='total_pages'
+
+      this.TableData=res[results]
+      this.TotalImages=res[total]
+      this.Pages=res[total_pages]
+
+    }
+  )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Materials
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
